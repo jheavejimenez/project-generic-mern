60a78299e3602dd7c15b89e8aa84972dccbc0006
Jheave <jimenezjheave123@gmail.com>
2021-07-14 21:16:53 +0800

    added routes for users

--- backend/routes/users.route.js
+++ backend/routes/users.route.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
 let User = require('../models/users.model');
 
-router.route('/').get((req, res) => {
-  User.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
+router.get('/', async (req, res) =>  {
+  try{
+    const user = await User.find();
+    res.json(user);
+
+  }catch(err){
+    res.status(400).json('error')
+
+  }
 });
 
 router.route('/add').post((req, res) => {
@@ -15,6 +20,34 @@ router.route('/add').post((req, res) => {
   newUser.save()
     .then(() => res.json('User added!'))
     .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.put('/update/:id', async (req, res) => {
+  try{
+    const update = {
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+
+    }
+
+  }catch(err){
+    res.status(400).json('error')
+
+  }
+  
+});
+
+router.delete('/delete/:id', async (req, res) =>{
+  try{
+    await User.findByIdAndDelete(req.params.id)
+    res.json('Generic deleted!')
+
+  }catch(err){
+    res.status(400).json('error')
+    
+  }
+
 });
 
 module.exports = router;
